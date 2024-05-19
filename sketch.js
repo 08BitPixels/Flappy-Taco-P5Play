c61928e9fbd9df84284abf3dcb50c40173905676
Rohan Garner <161069703+08BitPixels@users.noreply.github.com>
2024-05-19 19:34:36 +0100

    Update sketch.js, fork.png
    - Changed the fork image to include multiple forks on the same level
    - Started coding the new fork positioning mechanism

--- sketch.js
+++ sketch.js
@@ -22,7 +22,10 @@ let player_jump_boost;
 
 // Forks
 let forks;
-let fork_scale;
+let fork_align_turn;
+let fork_round;
+let fork_scale_x;
+let fork_scale_y;
 let fork_img;
 let fork_height;
 let fork_width;
@@ -57,32 +60,36 @@ function setup() {
     player = new Sprite(500, CENTER_Y);
     
     player.img = player_img;
-    player.collider = 'none';
-    player.addCollider(0, 0, 500, 300);
-    player.collider = 'd';
+    player.collider = 'k';
     player.layer = 1;
     player.scale = 0.3;
-    player.debug = true;
 
     // Forks
+	fork_round = 0;
+	fork_align_turn = 'up';
     fork_count = 4;
     fork_speed = 5;
     fork_gap = 400;
-    fork_scale = 2;
-    fork_height = 351 * fork_scale;
-    fork_width = 69 * fork_scale;
-    fork_offset_y_bounds = [300, 700];
+    fork_scale_y = 2;
+	fork_scale_x = 2.5;
+    fork_height = 351 * fork_scale_y;
+    fork_width = 188 * fork_scale_x;
+    fork_offset_y_bounds = [400, 600];
     
     forks = new Group();
     
     forks.vel.x = -fork_speed;
-    forks.debug = true;
-    forks.layer = 1;
-    forks.collider = 'none';
-
-    player.overlaps(forks, collide);
+    forks.collider = 'k';
     
-    for (let i = 0; i < fork_count; i++) {add_forks(x = WIDTH + (WIDTH / fork_count) * i + fork_width / 2, img = fork_img)}
+    for (let i = 0; i < fork_count; i++) {
+
+		add_forks(
+			x = WIDTH + (WIDTH / fork_count) * i + fork_width / 2,
+			id = i,
+			img = fork_img
+		)
+	
+	}
 
     // Text
     score_txt = new Sprite(100, 50, 160, 60);
@@ -120,7 +127,12 @@ function draw() {
         let fork1 = forks[i - 1];
         let fork2 = forks[i];
 
-        if (fork1.x <= 0 - fork_width / 2 || fork2.x <= 0 - fork_width / 2) {position_forks(WIDTH - fork_width / 2, fork1, fork2)}
+        if (fork1.x <= 0 - fork_width / 2 || fork2.x <= 0 - fork_width / 2) {
+
+			if (fork1.id == 0 || fork2.id == 0) {fork_round++}
+			position_forks(WIDTH - fork_width / 2, fork1.id, fork1, fork2)
+
+		}
         
     }
 
@@ -135,10 +147,6 @@ function input() {
 
 	}
 	if (mouse.presses()) {jump()}
-}
-
-function collide(player, fork) {
-	console.log('hit');
 }
 
 function update_score() {
@@ -168,39 +176,85 @@ function fall() {
 }
 
 // FORKS
-function add_forks(x, img) {
+function add_forks(x, id, img) {
 
     let fork1 = new forks.Sprite(0, 0);
     fork1.img = img;
-    fork1.scale = fork_scale;
-    fork1.addCollider(0, 0, 48 * fork_scale, 351 * fork_scale);
+    fork1.scale.y = fork_scale_y;
+	fork1.scale.x = fork_scale_x;
 	fork1.collider = 'k';
     fork1.passed = false;
+	fork1.id = id;
+	fork1.p_id = id;
     fork1.orientation = 'up';
 
     let fork2 = new forks.Sprite(0, 0);
     fork2.img = img;
-    fork2.scale = fork_scale;
-    fork2.addCollider(0, 0, 48 * fork_scale, 351 * fork_scale);
+    fork2.scale.y = fork_scale_y;
+	fork2.scale.x = fork_scale_x;
 	fork2.collider = 'k';
     fork2.rotation = 180
     fork2.passed = false
+	fork2.id = id;
+	fork2.p_id = id + 1;
     fork2.orientation = 'down'
 
-    position_forks(x, fork1, fork2)
+    position_forks(x, id, fork1, fork2)
     
 }
 
-function position_forks(x, fork1, fork2) {
+function position_forks(x, id, fork1, fork2) {
 
-    fork_offset_y = random(fork_offset_y_bounds[0], fork_offset_y_bounds[1]);
-    
-    fork1.x = x;
-    fork1.y = fork_offset_y + fork_height / 2 + fork_gap / 2
-    fork1.passed = false
+	console.log(id, fork_round, (id == 0 && fork_round == 0), fork_align_turn)
 
-    fork2.x = x;
-    fork2.y = fork_offset_y - fork_height / 2 - fork_gap / 2
-    fork2.passed = false
+	if (id == 0 && fork_round == 0) {
+
+		fork_offset_y = random(fork_offset_y_bounds[0], fork_offset_y_bounds[1]);
+		
+		fork1.x = x;
+		fork1.y = fork_offset_y + fork_height / 2 + fork_gap / 2;
+		fork1.passed = false;
+
+		fork2.x = x;
+		fork2.y = fork_offset_y - fork_height / 2 - fork_gap / 2;
+		fork2.passed = false;
+
+		fork_align_turn = 'down';
+
+	}
+	else {
+
+		fork_offset_y = random(fork_offset_y_bounds[0], fork_offset_y_bounds[1]);
+
+		if (fork_align_turn == 'up') {
+
+			console.log(fork1.p_id, fork2.p_id)
+
+			fork1.x = x; 
+			fork1.y = forks[fork1.p_id + (forks.length - 2) % forks.length].y;
+			fork1.passed = false;
+
+			fork2.x = x;
+			fork2.y = fork_offset_y - fork_height / 2 - fork_gap / 2;
+			fork2.passed = false;
+
+			fork_align_turn = 'down';
+
+		}
+		else if (fork_align_turn == 'down') {
+
+			fork1.x = x;
+			fork1.y = fork_offset_y + fork_height / 2 + fork_gap / 2;
+			fork1.passed = false;
+
+			fork2.x = x;
+			fork2.y = forks[fork2.p_id + (forks.length - 2) % forks.length].y;
+			fork2.passed = false;
+
+			fork_align_turn = 'up';
+
+		}
+
+	}
     
 }
